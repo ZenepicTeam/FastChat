246b6653a6723a4748eb82567eeaba1a32bcb15e
Yachuan <yachuan.zhang@zenjoy.net>
2023-10-04 23:03:46 +0800

    MythoMaxAdapter should use its own model and tokenizer

--- fastchat/model/model_adapter.py
+++ fastchat/model/model_adapter.py
@@ -687,6 +687,12 @@ class MythoMaxAdapter(BaseModelAdapter):
     def match(self, model_path: str):
         return "mythomax" in model_path.lower()
     
+    def load_model(self, model_path: str, from_pretrained_kwargs: dict):
+        model, tokenizer = super().load_model(model_path, from_pretrained_kwargs)
+        model.config.eos_token_id = tokenizer.eos_token_id
+        model.config.pad_token_id = tokenizer.pad_token_id
+        return model, tokenizer
+    
     def get_default_conv_template(self, model_path: str) -> Conversation:
         return get_conv_template("mythomax")
     
